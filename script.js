6ebf426fbb563fe3c56d361ebcb03df1ffc6decb
sk00l <saurav.bhattarai078@gmail.com>
2023-01-16 10:28:43 +0545

    optimized the script for more smotheness

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 const images = document.getElementsByClassName("image");
-
-let globalIndex = 0,
-    last = { x: 0, y: 0 };
+let globalIndex = 0;
+let last = { x: 0, y: 0 };
 
 const activate = (image, x, y) => {
   image.style.left = `${x}px`;
   image.style.top = `${y}px`;
   image.style.zIndex = globalIndex;
-
   image.dataset.status = "active";
-
   last = { x, y };
 }
 
-const distanceFromLast = (x, y) => {
-  return Math.hypot(x - last.x, y - last.y);
-}
-
 const handleOnMove = e => {
-  if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+  const x = e.clientX, y = e.clientY;
+  const distance = Math.hypot(x - last.x, y - last.y);
+  if(distance > (window.innerWidth / 20)) {
     const lead = images[globalIndex % images.length],
           tail = images[(globalIndex - 5) % images.length];
-
-    activate(lead, e.clientX, e.clientY);
-
+    activate(lead, x, y);
     if(tail) tail.dataset.status = "inactive";
-    
     globalIndex++;
+    last = {x, y}
+    requestAnimationFrame(handleOnMove);
   }
 }
 
 window.onmousemove = e => handleOnMove(e);
-
 window.ontouchmove = e => handleOnMove(e.touches[0]);
